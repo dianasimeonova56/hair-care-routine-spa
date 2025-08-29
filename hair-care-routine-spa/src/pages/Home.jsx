0802122ee0634a60285a19f600b36a4510904b50
dianasim <diana.simeonova56@gmail.com>
2025-08-29 22:15:21 +0300

    fix

--- hair-care-routine-spa/src/pages/Home.jsx
+++ hair-care-routine-spa/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import '../../assets/home.css'
+import '../assets/home.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,9 +10,11 @@ const Home = () => {
       </div>
       <div className="btn-group">
         <h1>Build a self care routine suitable for you</h1>
-        <p>Take out test to get a personalised self care <br/>
-        routine based on your needs.</p>
-        <button>Start the quiz</button>
+        <p>Take out test to get a personalised self care <br />
+          routine based on your needs.</p>
+        <Link to="/q1">
+          <button>Start the quiz</button>
+        </Link>
       </div>
     </div>
   )
