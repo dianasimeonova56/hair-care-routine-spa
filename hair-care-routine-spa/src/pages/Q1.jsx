473ff17b44b67eb8e0d415b3bc8122c5cd97d108
dianasim <diana.simeonova56@gmail.com>
2025-08-29 22:15:39 +0300

    add q1 page and stary question css

---
+++ hair-care-routine-spa/src/pages/Q1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Q1 = () => {
+  return (
+    <div className="container">
+      <div className="question">
+        <h1>What's your hair type or texture?</h1>
+      </div>
+      <div className="btn-group">
+       
+      </div>
+    </div>
+  )
+}
+
+export default Q1  
