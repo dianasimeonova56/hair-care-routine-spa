c64b8c649f45dadaec90b3c04aa20ed763e9961a
dianasim <diana.simeonova56@gmail.com>
2025-08-29 22:14:51 +0300

    fix imports and add route for q1

--- hair-care-routine-spa/src/App.jsx
+++ hair-care-routine-spa/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
+import Home from './pages/Home';
+import Q1 from './pages/Q1';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/q1" element={<Q1 />} />
       </Routes>
     </Router>
   )
