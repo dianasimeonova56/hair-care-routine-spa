06079457e243dd434c169969c231d18635de4e67
dianasim <diana.simeonova56@gmail.com>
2025-08-29 23:40:17 +0300

    switch some classes and component use in home page

--- hair-care-routine-spa/src/pages/Home.jsx
+++ hair-care-routine-spa/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../assets/home.css'
 import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const Home = () => {
   return (
@@ -8,12 +9,13 @@ const Home = () => {
       <div className="hero-img">
         <img src="/images/spa.jpg" />
       </div>
-      <div className="btn-group">
+      <div className="group">
         <h1>Build a self care routine suitable for you</h1>
         <p>Take out test to get a personalised self care <br />
           routine based on your needs.</p>
         <Link to="/q1">
-          <button>Start the quiz</button>
+          {/* <Button text="Start the quiz"></Button> */}
+          <button className='btn-start'>Start the quiz</button>
         </Link>
       </div>
     </div>
