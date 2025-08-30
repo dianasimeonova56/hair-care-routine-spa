4d70a202f7aff66badc5c71404beb1fe4fa80923
dianasim <diana.simeonova56@gmail.com>
2025-08-30 11:42:59 +0300

    add circular progress bar in question component, style it according to design

--- hair-care-routine-spa/src/pages/Q1.jsx
+++ hair-care-routine-spa/src/pages/Q1.jsx
@@ -3,6 +3,8 @@ import '../assets/question.css'
 import Option from '../components/Option'
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
+import { CircularProgressbar, buildStyles  } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css";
 
 const options = [
     {
@@ -26,21 +28,34 @@ const options = [
 const Q1 = () => {
     return (
         <div className="container">
-            <div className="question">
-                <h1>What's your hair type or texture?</h1>
+            <div className="main-part">
+                <div className="question">
+                    <h1>What's your hair type or texture?</h1>
+                </div>
+                <div className="options-group">
+                    {options.map((option) => (
+                        <Option key={option.id} id={option.id} text={option.text} />
+                    ))}
+                </div>
+                <div className="btn-group">
+                    <Link to={'..'} className="back">
+                        <p>Back</p>
+                    </Link>
+                    <Link to='/q2'>
+                        <Button text="Next question →"></Button>
+                    </Link>
+                </div>
             </div>
-            <div className="options-group">
-                {options.map((option) => (
-                    <Option key={option.id} id={option.id} text={option.text} />
-                ))}
-            </div>
-            <div className="btn-group">
-                <Link to={'..'} className="back">
-                   <p>Back</p>
-                </Link>
-                <Link to='/q2'>
-                    <Button text="Next question →"></Button>
-                </Link>
+            <div className="aside">
+                <CircularProgressbar 
+                    value={`${(1/5) * 100}`}
+                    text={`${1}/5`}
+                    strokeWidth={5}
+                    styles={buildStyles({
+                        pathColor: `#AADDF3`,
+                        trailColor: '#EEF7FB',
+                        textColor: '#1C2635'
+                    })}></CircularProgressbar>
             </div>
         </div>
     )
