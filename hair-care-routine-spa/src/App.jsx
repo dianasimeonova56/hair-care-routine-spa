6f04eb8fde89d42a22b2e0797b34213c8f2eaa4e
dianasim <diana.simeonova56@gmail.com>
2025-08-30 19:59:44 +0300

    add centralized state to memorize answers, add state for navigating back to prev page and delete the selected answer

--- hair-care-routine-spa/src/App.jsx
+++ hair-care-routine-spa/src/App.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import QuestionPage from './components/QuestionPage';
+import Result from './pages/Result'
 
 const App = () => {
+  const [answers, setAnswers] = useState({});
+
+  const handleAnswer = (questionId, selectedOption) => {
+    setAnswers(prev => ({ ...prev, [questionId]: selectedOption }));
+  };
+
+  const handleGoBack = (questionId,) => {
+    if (questionId != undefined) {
+      setAnswers(prev => {
+        const updated = { ...prev };
+        delete updated[questionId];
+        return updated;
+      });
+    }
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/q/:id" element={<QuestionPage />} />
+        <Route path="/q/:id" element={<QuestionPage answers={answers} onAnswer={handleAnswer} onGoBack={handleGoBack} />} />
+        <Route path="/result" element={<Result answers={answers} />} />
       </Routes>
     </Router>
   )
