9e27f49cef1a750ad371b9a51eac7e36363eb71e
dianasim <diana.simeonova56@gmail.com>
2025-08-31 22:30:55 +0300

    add heart pngs for wishlist btn, make hook for hovering over it

--- hair-care-routine-spa/src/components/Card.jsx
+++ hair-care-routine-spa/src/components/Card.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../assets/card.css'
 
-function Card({product}) {
+function Card({ product }) {
+  const mainIcon = '/images/heart.png';
+  const filledIcon = '/images/heart-filled.png';
+
+  const isWishlist = false;
+
+  const [activeIcon, setActiveIcon] = useState(mainIcon);
+
   return (
     <div className="card">
-        <div className='card-img'>
-            <img src={product.images[0].src}/>
+      <div className='card-img'>
+        <div className='fav'>
+          <img
+            src={activeIcon}
+            onMouseOver={() => setActiveIcon(filledIcon)}
+            onMouseLeave={() => setActiveIcon(mainIcon)}
+          />
         </div>
-        <div className='info-card'>
-            <h2>{product.title}</h2>
-            <p>${product.variants[0].price}</p>
+        <div className='img'>
+          <img src={product.images[0].src} />
         </div>
+      </div>
+      <div className='info-card'>
+        <h2>{product.title}</h2>
+        <p>${product.variants[0].price}</p>
+      </div>
     </div>
   )
 }
